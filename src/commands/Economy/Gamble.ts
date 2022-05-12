6ef37966cc4edeb7b40e2f157487c5d2c01a01a9
doruk-backups01 <105433152+doruk-backups01@users.noreply.github.com>
2022-05-12 18:21:50 +0530

    Update Gamble.ts

--- src/commands/Economy/Gamble.ts
+++ src/commands/Economy/Gamble.ts
@@ -23,7 +23,7 @@ export default class Command extends BaseCommand {
     { joined }: IParsedArgs
   ): Promise<void> => {
     /*eslint-disable @typescript-eslint/no-explicit-any*/
-    if (!(await this.client.getGroupData(M.from)).cease)
+    if (!M.from !== "@g.us")
       return void M.reply(
         `You can't gamble here. Use ${this.client.config.prefix}support to get casino group link.`
       );
